5ed323ea60de332dcf12915e44055a55045c78cd
Achref Ben Ghali <achrefbenghali702@.com>
2022-12-07 11:59:44 +0100

    schema

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,10 +1,21 @@
 
+import {db} from '../db.js'
+import bcrypt from 'bcryptjs';
 
 
 export const register =(req, res)=>{
-
+const q= "SELECT * users WHERE email= ? OR username= ?";
+db.query(q,[req.body.email,req.body.name],(err,data)=>{
+    if(err){
+        res.json(err);
+    }
+    if(data.length){
+        res.status(409).json("user already exists");
+    }
+})
     //nthabtou  ke fama existing user
-    
+    const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(req.body.password, salt);
     }
     export const login =(req, res)=>{
     
