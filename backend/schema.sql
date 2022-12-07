4ffcc786871af9d7ef1b4a748eaa14c39e6d453b
Achref Ben Ghali <achrefbenghali702@.com>
2022-12-07 13:01:59 +0100

    schema added

---
+++ backend/schema.sql
@@ -0,0 +1,60 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema blog
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema blog
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `blog` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `blog` ;
+
+-- -----------------------------------------------------
+-- Table `blog`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blog`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `img` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `blog`.`posts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `blog`.`posts` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `desc` VARCHAR(2000) NOT NULL,
+  `img` VARCHAR(255) NOT NULL,
+  `userid` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `userid_idx` (`userid` ASC) VISIBLE,
+  CONSTRAINT `userid`
+    FOREIGN KEY (`userid`)
+    REFERENCES `blog`.`users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
